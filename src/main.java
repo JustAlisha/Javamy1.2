5cbffc240afe5ca7912f9b32378b3a1b0329a91c
Test <bloodreighn965@gmail.com>
2024-02-05 21:17:21 +0300

    1

---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int initialBalance =300 ;
+        int addend = 1300 ;
+
+        int bonus = 0;
+        if(addend>1000) {;
+            bonus=addend/100 ;
+        }
+        //int bonus = addend>100?addend/100 : 0;
+        int finalBalance = initialBalance + addend +bonus;
+        System.out.println("Итоговый баланс" + finalBalance );
+        System.out.println("Бонус:" + bonus)
+
+
+
+    }
+}
